a0548ecdeb6377c98e5b7a03e09ae1c8ea52a20b
Vishal14081997 <1408vishalsingh@gmail.com>
2026-07-28 17:25:07 +0530

    control flow statements

--- js/26-jan.js
+++ js/26-jan.js
@@ -1,3 +1,4 @@
+
 // not equal 
 // let firstVariable = 10;
 // let secondVariable = 20;
@@ -8,6 +9,19 @@
 // let secondVariable = 10;
 // console.log(firstVariable != secondVariable);
 
-let firstVariable = "10";
-let secondVariable = 10;
-console.log(firstVariable != secondVariable);
+// let firstVariable = "10";
+// let secondVariable = 10;
+// console.log(firstVariable != secondVariable);
+
+// let firstVariable = 100;
+// let secondVariable = 99;
+// console.log(firstVariable >= secondVariable);
+
+// bitwise operator (6)--------
+// && || ! 
+// & | ^ << >> ~
+
+let firstNumber = 5; // base -10  binary no of 5 - 101
+let secondNumber = 8; // base - 2
+// console.log(firstNumber | secondNumber);
+console.log(thirdVariable & 1 === 0 );
---
+++ js/28-jan.js
@@ -0,0 +1,97 @@
+// control flow statements----------
+
+// conditional statements 
+// loops
+
+// if-else 
+// switch cases
+
+// let number = 30;
+// if (number >= 30) {
+//     console.log("the number is grater than 30");
+// }
+
+// let number = 29;
+// if (number % 2 === 0) {
+//     console.log("the number is even");
+// } else {
+//     console.log("the number is odd");
+// }
+
+// this is a wrong methods 
+// let marks = 83;
+// if (marks >= 90) {
+//     console.log("Grade : A+");
+// } else if (marks >= 70) {
+//     console.log("Grade : B")
+// } else if (marks >= 80) {
+//     console.log("Grade : A")
+// } else if (marks >= 60) {
+//     console.log("Grade : C")
+// } else if (marks >= 50) {
+//     console.log("Grade : D")
+// } else {
+//     console.log("Grade : F")
+// }
+
+
+// let marks = 83;
+// if (marks <= 100 && marks >= 90) {
+//     console.log("Grade : A+");
+// } else if (marks < 90 && marks >= 80) {
+//     console.log("Grade : A")
+// } else if (marks < 80 && marks >= 70) {
+//     console.log("Grade : B")
+// } else if (marks < 70 && marks >= 60) {
+//     console.log("Grade : C")
+// } else if (marks < 60 && marks >= 50) {
+//     console.log("Grade : D")
+// } else if (marks < 50) {
+//     console.log("Grade : F")
+// } else{
+//     console.log("Invalid marks");
+// }
+
+// switch cases -----------
+
+// let day = "Monday";
+// switch (day) {
+//     case "Monday":
+//         console.log("first day of the week");
+//         break;
+//     case "Tuesday":
+//         console.log("second day of the week");
+//         break;
+//     case "Wednesday":
+//         console.log("third day of the week");
+//         break;
+//     case "Thrusday":
+//         console.log("fourth day of the week");
+//         break;
+//     case "Friday":
+//         console.log("fifth day of the week");
+//         break;
+//     case "Saturday":
+//         console.log(" sixth day of the week");
+//         break;
+//     case "Sunday":
+//         console.log("seventh day of the week");
+//         break;
+//     default: console.log("Invalid Day");
+
+// }
+
+// Ternory Operator -----------
+
+// let marks = 49;
+
+// marks >= 90 ? console.log("Grade : A+"):console.log("Grade Something");
+
+// marks <= 100 && marks >= 90 ? console.log("Grade : A+") : marks < 90 && marks >= 80 ? console.log("Grade : A") : marks < 80 && marks >= 70 ? console.log("Grade : B") : marks < 70 && marks >= 60 ? console.log("Grade : C") : marks < 60 && marks >= 50 ? console.log("Grade : D") : marks < 50 ? console.log("Grade : F"):console.log("Grade Something")
+
+
+// loops - 
+
+// for-for , forin , forof ,while , do while
+
+
