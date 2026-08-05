7c95cecd35efc380314154f26b1f33a3bbaefd04
Vishal14081997 <1408vishalsingh@gmail.com>
2026-08-05 07:27:23 +0530

    update

--- js/31-jan.js
+++ js/31-jan.js
@@ -3,9 +3,18 @@
 
 //! function which can be passed in other function parameter or argument
 
+// function greet(name, callback) {
+//     console.log("Hello " + name);
+//     callback(); // callback function call
+// }
+// function sayBye() {
+//     console.log("Good Bye!");
+// }
+// greet("Vishal", sayBye);
+
+
 // function sum(callback) {
 //     console.log("the console is line" , callback);
-
 //     callback();
 // }
 // function sumCallback(){
@@ -34,7 +43,6 @@
 // console.log(addOfNumbers);
 
 
-
 //! Q. write a funtion processNumbers that takes an array of numbers and a callback. the callback should add 5 to each number and return the update array.
 
 // function processNumbers(arr, callback) {
@@ -53,30 +61,30 @@
 
 //! write a funtion that takes an array and return the sum of all the elements of array.
 
-let cartData = [
-    {
-        productName: "Oneplus",
-        productPrice: 299
-    },
-    {
-        productName: "Sumsung",
-        productPrice: 2999
-    },
-    {
-        productName: "Noting",
-        productPrice: 399
-    },
-    {
-        productName: "Iphone",
-        productPrice: 299
-    }
-]
-function totalOfCartPrice(data) {
-    let total = 0
-    for (let i = 0; i < data.length; i++) {
-        total += data[i].productPrice
-    }
-    return total;
-}
-let totalpayment = totalOfCartPrice(cartData)
-console.log(totalpayment);
+// let cartData = [
+//     {
+//         productName: "Oneplus",
+//         productPrice: 299
+//     },
+//     {
+//         productName: "Sumsung",
+//         productPrice: 2999
+//     },
+//     {
+//         productName: "Noting",
+//         productPrice: 399
+//     },
+//     {
+//         productName: "Iphone",
+//         productPrice: 299
+//     }
+// ]
+// function totalOfCartPrice(data) {
+//     let total = 0
+//     for (let i = 0; i < data.length; i++) {
+//         total += data[i].productPrice
+//     }
+//     return total;
+// }
+// let totalpayment = totalOfCartPrice(cartData)
+// console.log(totalpayment);
